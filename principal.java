dd6466036e35c6c3bc71bffac6f12aea2c4e99f3
Patricio23Cadena <kevin23cadena@hotmail.com>
2020-08-04 22:51:51 -0500

    comentarioprueba

--- principal.java
+++ principal.java
@@ -1,7 +1,7 @@
 package ejemplo2Sinch;
 
 public class principal {
-	
+
 	public static void main(String [] args) {
 		proceso hilo1 = new proceso("Hilo1");
 		proceso2 hilo2 = new proceso2(hilo1);
@@ -9,5 +9,6 @@ public class principal {
 		hilo1.start();
 		hilo2.start();
 		hilo3.start();
+		//asdfasdf
 	}
 }
